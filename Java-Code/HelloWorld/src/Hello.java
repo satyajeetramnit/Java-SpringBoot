e815313167e670481daeeba530e3e2705a048988
satyajeetramnit <1906130@kiit.ac.in>
2023-07-14 21:47:39 +0530

    Getting started  with java basics

---
+++ Java-Code/HelloWorld/src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        int num1=10;
+        int num2=20;
+        int num3=30;
+        int numt=num1+num2+num3;
+        int num1000=10-numt;
+        System.out.println(num1000);
+    }
+}
